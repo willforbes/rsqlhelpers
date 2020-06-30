29df01b380cb387e2d4c1d8754f4e61c4e730ef9
William Forbes <William.Forbes@lta.org.uk>
2020-06-30 15:09:40 +0100

    Modified safeSQLVar function to allow for vectors

--- R/helpers.R
+++ R/helpers.R
@@ -9,15 +9,21 @@
 #' safeSQLVar("unsafe' string")
 safeSQLVar <- function(var) {
 
-  if (is.na(var)) {
-    return("NULL")
-  }else if (is.character(var)) {
-    return(paste("'", gsub("'", "\\\\'", var), "'", sep = ""))
-  }else if (is.numeric(var)) {
-    return(as.character(var))
-  }else if (is.double(var)) {
-    return(paste("'", as.character(var), "'", sep = ""))
-  }else {
-    return(var)
+  ret_vec <- rep("", length(var))
+
+  for (i in 1:length(var)) {
+    if (is.na(var[i])) {
+      ret_vec[i] <- "NULL"
+    } else if (is.character(var[i])) {
+      ret_vec[i] <- paste("'", gsub("'", "\\\\'", var[i]), "'", sep = "")
+    } else if (is.numeric(var[i])) {
+      ret_vec[i] <- as.character(var[i])
+    } else if (is.double(var[i])) {
+      ret_vec[i] <- paste("'", as.character(var[i]), "'", sep = "")
+    } else {
+      ret_vec[i] <- var[i]
+    }
   }
+
+  return(ret_vec)
 }
